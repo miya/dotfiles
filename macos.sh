d3fef340128f4df6fb6c9855d2c1a4044242ac00
Shun Miyazawa <shun-m@weseek.co.jp>
2021-12-29 21:56:39 +0900

    add macos.sh

---
+++ macos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#  /'\_/`\                 /\  __`\
+# /\      \     __      ___\ \ \/\ \    ____
+# \ \ \__\ \  /'__`\   /'___\ \ \ \ \  /',__\
+#  \ \ \_/\ \/\ \L\.\_/\ \__/\ \ \_\ \/\__, `\
+#   \ \_\\ \_\ \__/.\_\ \____\\ \_____\/\____/
+#    \/_/ \/_/\/__/\/_/\/____/ \/_____/\/___/
+
+
+# setup screenshot
+mkdir ~/Pictures/ScreenShots/
+defaults write com.apple.screencapture location ~/Pictures/ScreenShots/
+defaults write com.apple.screencapture name "ScreenShot"
